775f376d8d044c15d2fc42770a08e15b0f22b311
Kenan Klepic <kenan.klepic@gmail.com>
2023-10-31 08:14:45 +0100

    Commit

--- test/specs/invalidPasswordLoginTest.js
+++ test/specs/invalidPasswordLoginTest.js
@@ -3,7 +3,7 @@ const LoginPage = require('../pageobjects/login.page')
 const SecurePage = require('../pageobjects/secure.page')
 
 describe('Login with invalid password', () => {
-    it('should login with valid credentials', async () => {
+    it('should not login with invalid credentials', async () => {
         await LoginPage.open()
 
         await LoginPage.login('tomsmith', '123')
